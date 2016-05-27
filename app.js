8648877b7b26e23a9499045fa4a0bb8995bc7e19
Matt Ellen <matt@matthewellen.co.uk>
2016-05-27 19:54:45 +0100

    got the layout to be correct including space for the answer

--- app.js
+++ app.js
@@ -29,7 +29,10 @@
 		    [
 			'$scope',
 			function($scope)
-			{
+			{			    
+			    $scope.firstNumber = $scope.firstNumber.trim();
+			    $scope.secondNumber = $scope.secondNumber.trim();
+			    
 			    this.firstNumberParts = $scope.firstNumber.split('');
 			    this.secondNumberParts = $scope.secondNumber.split('');
 			    
@@ -37,16 +40,42 @@
 
 			    this.steps = [];
 
-			    this.n1predot = calculatePreDot($scope.firstNumber, $scope.secondNumber);
 			    this.n1postdot = calculatePostDot($scope.firstNumber, $scope.secondNumber);
-			    this.n2predot = calculatePreDot($scope.secondNumber, $scope.firstNumber);
 			    this.n2postdot = calculatePostDot($scope.secondNumber, $scope.firstNumber);
 
-			    if(this.n1predot > this.n2predot)
+			    this.answerCharCount = $scope.firstNumber.match(/\d/g).length + $scope.secondNumber.match(/\d/g).length;
+
+			    if($scope.firstNumber.indexOf('.') > -1 || $scope.secondNumber.indexOf('.') > -1)
 			    {
-				this.n1predot += 1;
+				this.answerCharCount++;
 			    }
 
+			    if($scope.firstNumber.indexOf('.') > -1 && $scope.secondNumber.indexOf('.') === -1)
+			    {
+				this.n2postdot++;
+			    }
+
+			    if($scope.firstNumber.indexOf('.') === -1 && $scope.secondNumber.indexOf('.') > -1)
+			    {
+				this.n1postdot++;
+			    }
+
+			    if(($scope.firstNumber[0] === '-' && $scope.secondNumber[0] !== '-') && ($scope.firstNumber[0] !== '-' && $scope.secondNumber[0] === '-'))
+			    {
+				this.answerCharCount++;
+				this.isNegative = true;
+			    }
+
+			    this.generateRange = function(size)
+			    {
+				return new Array(size);
+			    };
+			    
+			    this.underlineRange = function()
+			    {
+				return new Array(this.answerCharCount + 2);
+			    };
+			    
 			    function calculatePostDot(n1, n2)
 			    {
 				var span = 0;
@@ -68,53 +97,9 @@
 				{
 				    span = n2pd - n1pd;
 				}
-
-				console.log(span);
-				
+			
 				return span;
 			    }
-
-			    function calculatePreDot(n1, n2)
-			    {
-				var span = 0;
-
-				var n1pd = 0;
-				var n2pd = 0;
-
-				if(n1.indexOf('.') > -1)
-				{
-				    n1pd = n1.split('.')[0].length;
-				}
-				else
-				{
-				    n1pd = n1.length;
-				}
-
-				if(n2.indexOf('.') > -1)
-				{
-				    n2pd = n2.split('.')[0].length;
-				}
-				else
-				{
-				    n2pd = n2.length;
-				}
-
-				if(n1pd < n2pd)
-				{
-				    span = n2pd - n1pd;
-				}
-
-				console.log(span);
-				
-				return span;
-			    }
-
-			    this.step = function()
-			    {
-				
-			    };
-			    
-			}
 		    ]
 		   );
      
